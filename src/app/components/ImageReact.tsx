1c2afe7ec34eec191f0f9f1ce9c6b53f7816cab6
Ashish kuldeep <ashishkuldeep6@gmail.com>
2025-08-24 20:51:11 +0530

    Check now url on remote

--- src/app/components/ImageReact.tsx
+++ src/app/components/ImageReact.tsx
@@ -1,66 +1,60 @@
-
-import React, { useState } from 'react'
-import { FiLoader } from 'react-icons/fi'
+import React, { useState } from "react";
+import { FiLoader } from "react-icons/fi";
 
 const ImageReact = ({
-    src,
-    className,
-    alt,
-    width,
-    onMouseMove,
-    style,
-    onClick
+  src,
+  className,
+  alt,
+  width,
+  onMouseMove,
+  style,
+  onClick,
 }: {
-    src: string,
-    className?: string,
-    alt?: string,
-    width?: number,
-    height?: number
-    onMouseMove?: React.MouseEventHandler<HTMLImageElement> | undefined,
-    style?: object
-    onClick?: Function
+  src: string;
+  className?: string;
+  alt?: string;
+  width?: number;
+  height?: number;
+  onMouseMove?: React.MouseEventHandler<HTMLImageElement> | undefined;
+  style?: object;
+  onClick?: Function;
 }) => {
+  const [isLoaded, setIsLoaded] = useState<Boolean>(false);
 
-    const [isLoaded, setIsLoaded] = useState<Boolean>(false);
+  // console.log(style)
+  // console.log(className)
 
-    // console.log(style)
-    // console.log(className)
+  return (
+    <>
+      <span
+        className={className}
+        style={{ display: isLoaded ? "none" : "inline" }}
+      >
+        <FiLoader />
+      </span>
 
+      <img
+        onClick={(e: any | React.MouseEvent<HTMLImageElement, MouseEvent>) => {
+          onClick && onClick(e);
+        }}
+        width={width}
+        src={src.replace(/^http:\/\//i, "https://")}
+        alt={alt || ""}
+        className={`${className} `}
+        style={{
+          ...style,
+          display: isLoaded ? "inline" : "none",
+        }}
+        onLoad={() => setIsLoaded(true)}
+        onError={({ currentTarget }) => {
+          currentTarget.onerror = null; // prevents looping
+          currentTarget.src =
+            "https://res.cloudinary.com/dlvq8n2ca/image/upload/v1717510373/yqrqev3nq8yrbdkct3no.jpg";
+        }}
+        onMouseMove={onMouseMove}
+      />
+    </>
+  );
+};
 
-
-    return (
-
-        <>
-
-            <span
-                className={className}
-                style={{ display: isLoaded ? 'none' : 'inline' }}
-            >
-                <FiLoader />
-            </span>
-
-
-            <img
-                onClick={(e: any | React.MouseEvent<HTMLImageElement, MouseEvent>) => { onClick && onClick(e) }}
-                width={width}
-                src={src}
-                alt={alt || ""}
-                className={`${className} `}
-                style={{
-                    ...style,
-                    display: isLoaded ? 'inline' : 'none'
-                }}
-                onLoad={() => setIsLoaded(true)}
-                onError={({ currentTarget }) => {
-                    currentTarget.onerror = null; // prevents looping
-                    currentTarget.src = "https://res.cloudinary.com/dlvq8n2ca/image/upload/v1717510373/yqrqev3nq8yrbdkct3no.jpg";
-                }}
-                onMouseMove={onMouseMove}
-            />
-
-        </>
-
-    )
-}
-
-export default ImageReact
+export default ImageReact;
