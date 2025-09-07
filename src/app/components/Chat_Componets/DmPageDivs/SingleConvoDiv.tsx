a2fdfdd131e9b613df77205167f695812ebbcad0
Ashish kuldeep <ashishkuldeep6@gmail.com>
2025-09-07 23:51:08 +0530

    Active at last msg at and some added for single convo

--- src/app/components/Chat_Componets/DmPageDivs/SingleConvoDiv.tsx
+++ src/app/components/Chat_Componets/DmPageDivs/SingleConvoDiv.tsx
@@ -3,6 +3,7 @@ import { Conversation } from "@/types/chat-types";
 import Link from "next/link";
 import ImageReact from "../../ImageReact";
 import { decryptMessage } from "@/lib/Crypto-JS";
+import { formatDateToDDMMYYYY } from "@/helper/DateFomater";
 
 const SingleConvoDiv = ({
   convo,
@@ -24,20 +25,22 @@ const SingleConvoDiv = ({
         convoClickHandler && convoClickHandler(convo);
       }}
     >
-      <div className=" my-1 bg-sky-950 text-white min-h-16 max-h-28 rounded w-full flex items-center active:scale-90 hover:bg-sky-700 active:bg-sky-700 hover:cursor-pointer overflow-hidden ">
-        <span className=" relative max-w-10 min-w-10 h-10 max-h-10 min-h-10 w-10 p-[0.1rem] rounded-full mx-4 overflow-hidden">
+      <div className=" my-1 bg-sky-950 text-white min-h-16 max-h-28 rounded w-full flex gap-1 sm:gap-3 items-center flex-wrap active:scale-90 hover:bg-sky-700 active:bg-sky-700 hover:cursor-pointer overflow-hidden ">
+        <span className=" ml-2 relative max-w-14 min-w-14 h-14 max-h-14 min-h-14 w-14 p-[0.1rem] rounded-full overflow-hidden">
           <ImageReact
             className="  w-[99%] h-[99%] rounded-full object-cover object-center "
             src={convo?.avatar || ""}
           />
 
           {isOnline && (
-            <span className=" h-2.5 w-2.5 rounded-full border-2 border-black bg-green-500 absolute bottom-0.5 right-0.5 "></span>
+            <span className=" h-2.5 w-2.5 rounded-full border-2 border-black bg-green-500 absolute bottom-2 right-2 "></span>
           )}
         </span>
 
-        <div>
-          <p className=" text-lg font-semibold capitalize">{convo?.name}</p>
+        <div className=" flex flex-col justify-center mt-1.5">
+          <p className=" text-lg font-semibold capitalize leading-[0.85rem] ">
+            {convo?.name}
+          </p>
           <p className=" opacity-70 text-sm ">
             {convo?.lastMessage?.content
               ? `${decryptMessage(convo?.lastMessage?.content).slice(0, 40)} ${
@@ -47,6 +50,41 @@ const SingleConvoDiv = ({
                 } `
               : "Dare to Chat."}
           </p>
+
+          {convo?.type === "group" ? (
+            <div>
+              <p className=" opacity-70 text-[0.6rem] ">
+                {formatDateToDDMMYYYY(convo?.lastMessageAt)} (Last msg at)
+              </p>
+            </div>
+          ) : (
+            <div>
+              {!isOnline &&
+                convo.participants &&
+                convo?.participants.find(
+                  (user) =>
+                    user?.username?.toString() === convo?.name?.toString()
+                )?.lastSeen && (
+                  <p className=" opacity-70 text-[0.6rem] ">
+                    {formatDateToDDMMYYYY(
+                      convo?.participants.find(
+                        (user) =>
+                          user?.username?.toString() === convo?.name?.toString()
+                      )?.lastSeen
+                    )}{" "}
+                    (Active at)
+                  </p>
+                )}
+
+              {isOnline && (
+                <p className=" opacity-70 text-[0.6rem] ">
+                  <span className=" text-green-200 font-semibold ">Active</span>
+                </p>
+              )}
+            </div>
+          )}
+
+          {/* <p className=" opacity-70 text-[0.7rem] ">Active</p> */}
         </div>
       </div>
     </Link>
