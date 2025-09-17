939bae244405fb1043d407a0f9b5b25eb1c19f19
Ashish kuldeep <ashishkuldeep6@gmail.com>
2025-09-17 19:40:52 +0530

    Final PWA config and place in project

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -2,13 +2,13 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Next.js PWA",
-    short_name: "NextPWA",
-    description: "A Progressive Web App built with Next.js",
+    name: "Apostio",
+    short_name: "Apostio",
+    description: "A modern social platform ",
     start_url: "/",
     display: "standalone",
-    background_color: "#ffffff",
-    theme_color: "#000000",
+    background_color: "#000000",
+    theme_color: "#ffffff",
     icons: [
       {
         src: "https://res.cloudinary.com/dlvq8n2ca/image/upload/v1701708322/jual47jntd2lpkgx8mfx.png",
